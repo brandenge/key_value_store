02d39a35c9a566686b5bedd3d22d01f974488442
brandenge <86636108+brandenge@users.noreply.github.com>
2026-09-24 17:07:34 -0500

    Add kv_t.h

---
+++ inc/kv_t.h
@@ -0,0 +1,21 @@
+#ifndef KV_T_H
+#define KV_T_H
+
+typedef struct kv_entry_t {
+    char *key;
+    char *value;
+} kv_entry_t;
+
+typedef struct kv_t {
+    kv_entry_t *entries;
+    size_t capacity;
+    size_t count;
+} kv_t;
+
+kv_t *kv_init(size_t capacity);
+int    kv_put(kv_t *db, const char *key, const char *value);
+char  *kv_get(kv_t *db, const char *key);
+int    kv_delete(kv_t *db, const char *key);
+void   kv_free(kv_t *db);
+
+#endif
