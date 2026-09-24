96cdc5282ecc341124b84ae1797757799d831dde
brandenge <86636108+brandenge@users.noreply.github.com>
2026-09-24 17:06:28 -0500

    Add kv_t.c

---
+++ src/kv_t.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <kv_t.h>
+
+kv_t *kv_init(size_t capacity) {
+    kv_t *db = malloc(sizeof(kv_t));
+
+    if (db == NULL) {
+        perror("malloc");
+        return NULL;
+    }
+
+    kv_entry_t *entries = calloc(capacity, capacity * sizeof(kv_entry_t));
+
+    if (entries == NULL) {
+        perror("calloc");
+        free(db);
+        return NULL;
+    }
+
+    db->entries = entries;
+    db->capacity = capacity;
+    db->count = 0;
+    return db;
+}
